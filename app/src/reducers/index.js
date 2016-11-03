747e19a37775d5392b49988d0595966329e946e5
bsapaka <bws46@cornell.edu>
2016-11-02 20:41:45 -0500

    installed redux form

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
+import { reducer as formReducer } from 'redux-form'
 
 import auth from "./authReducer"
 import skills from "./skillsReducer"
@@ -6,6 +7,7 @@ import exps from "./expsReducer"
 // import references from "./referencesReducer"
 
 export default combineReducers({
+    form: formReducer,
     auth,
     skills,
     exps
